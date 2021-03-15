f9e4f3fbab9025496c5ee59a45631b86911c2247
YuYiHao <1293937984@qq.com>
2021-03-15 17:01:35 +0800

    修改了home组件

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
+  <el-container>
+      <el-header>Header
     <el-button type="info" @click="logout">退出</el-button>
-  </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+  </el-container>
 </template>
 
 <script>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Icon, Message } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Icon,
+  Message,
+  Container,
+  Header,
+  Main,
+  Aside
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Icon)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Aside)
 // 全局挂载登弹窗提示
 Vue.prototype.$message = Message
